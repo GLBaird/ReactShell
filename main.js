74f0b3e9ee82341664c884417db9e9ea7c8082f9
Leon Baird <leon.baird@qumo.do>
2017-09-01 15:45:59 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+import App from './src/App'
+
+let container = document.getElementById('container');
+
+ReactDom.render(<App />, container);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: {
+        app: './main.js'
+    },
+    output: {
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3000
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ['es2015', 'react', 'stage-0']
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
